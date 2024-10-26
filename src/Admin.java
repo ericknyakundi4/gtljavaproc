a2dbef974ed69d975a6a5b04d825b19e0b2ae426
Erick Nyakundi <ericknyakundi4@gmail.com>
2024-10-26 15:37:32 +0300

    set up

---
+++ src/Admin.java
@@ -0,0 +1,15 @@
+public class Admin extends Account
+{
+  private String authPassword;
+
+  public Admin(int _id, String _name, String _password) {
+      id = _id;
+      name = _name;
+      authPassword = _password;
+  }
+
+  public boolean authenticate(String password) {
+      return authPassword.equals(password);
+  }
+}
+
---
+++ src/DataSource.java
@@ -0,0 +1,7 @@
+public interface DataSource 
+{
+
+public void execute();
+
+}
+
